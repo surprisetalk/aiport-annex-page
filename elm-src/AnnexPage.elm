893402ba590bd8b1547490b1e63b06a4a7d2d32a
Taylor Sarrafian <surprisetalk@gmail.com>
2016-07-19 06:18:48 +0000

    progress

--- elm-src/AnnexPage.elm
+++ elm-src/AnnexPage.elm
@@ -1,8 +1,4 @@
 
--- TODO: we can use type Dict!!!!
--- TODO: split this up into different files under src
--- TODO: look at elm-markdown for how to parse html into the virtual-dom
-
 port module Main exposing (..)
     
 import Html exposing (..)
@@ -15,14 +11,15 @@ import String
 import Task
 import Http
             
+-- TODO: put the Json encoders inside modules, eg Pagelet.JsonDecode
 import Pagelet exposing (..)
 import Scrap exposing (..)
 import Page exposing (..)
 import Tree exposing (..)
 
 main : Program Never
-main =
-  Html.program
+main 
+= Html.program
     { init = init
     , view = view
     , update = update
@@ -42,160 +39,164 @@ main =
 
 -- MODEL
 
--- TODO: remove page and pagelet in favor of .active
--- TODO: page and pagelet are copies, which means we have to sync in a few places...
--- TODO: OR pagelets will have IDs, which means we can subscribe to the db :3
-type alias Model =
-    { pages : List (Page Msg)
+-- TODO: get it working without drag/drop first
+
+-- TODO: give pagelets own pile
+-- TODO: pages and pagelets have IDs in mongo, so let's save them by id?
+
+type alias Model 
+= { pages : List (Page Msg)
     , scraps : List (Scrap Msg)
-    , page : Page Msg
-    , pagelet : Pagelet Msg
-    , workspace : Html Msg
-    }
+    , workspace : 
+      { hoverscrap : Maybe (Scrap Msg)
+      , overpagelets : List (Scrap Msg)
+      , page : Maybe (Page Msg)
+      , pagelet : Maybe (Pagelet Msg)
+      }
+   }
     
 init : (Model, Cmd Msg)
-init =
-  ( { pages = testPages
-    , page = (List.head testPages) ? emptyPage 
-    , pagelet = emptyPagelet
-    , scraps = testScraps
-    , workspace = div [ styleColumn 50 "white" ] []
-    }
-  , fetchPages
+init 
+= ( { pages = []
+    , scraps = []
+    , workspace 
+    = { hoverscrap = Nothing
+      , overpagelets = []
+      , page = Nothing
+      , pagelet = Nothing
+      }
+    } 
+  -- TODO: fetch pages and scraps
+  , Cmd.none
   )
 
+localhost : String
+localhost 
+= "http://taysar.com:9097/"
+    
 
 -- UPDATE
 
+-- TODO: Scrap needs to change to function
+
 type Msg
-  = ViewPage (Page Msg)
+  = SelectPage (Page Msg)
   | SelectPagelet (Pagelet Msg)
-  | SelectScrap (Scrap Msg)
-  | FetchPageSucceed (List (Page Msg))
-  | FetchScrapSucceed (List (Html Msg))
-  | FetchFail Http.Error
+  | UpdatePage (Page Msg)
+  | UpdatePageName String
+  | UpdatePageRoute String
+  | SavePage (Page Msg)
+  | SavePagelet (Pagelet Msg)
+  | DragScrap (Scrap Msg) 
+  | DropScrap (Scrap Msg)
+  | MouseEnterPagelet (Pagelet Msg) 
+  | MouseLeavePagelet (Pagelet Msg)
+  | HttpSucceedSavePage (Html Msg)
+  | HttpSucceedFetchPages (List (Page Msg))
+  | HttpSucceedFetchScraps (List (Html Msg))
+  | HttpFail Http.Error
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    ViewPage page ->
+    SelectPage page ->
       ({ model | page = page }, Cmd.none)
     SelectPagelet pagelet ->
-      ({ model | pagelet = pagelet }, Cmd.none)
-    SelectScrap scrap ->
-      ({ model | pagelet = setScrap model.pagelet scrap }, Cmd.none)
+        let 
+            workspace = model.workspace
+        in 
+            ({ model | workspace = { workspace | pagelet = pagelet } }, Cmd.none)
+    -- TODO: make custom event handler onSubmit (Json.succeed (HtmlToPage children)) and put it on workspace area
+    UpdatePage page ->
+        let 
+            workspace = model.workspace
+            page = model.workspace.page
+        in 
+            ({ model | workspace = { workspace | page = page } }, Cmd.none)
+    UpdatePageName name ->
+        let 
+            workspace = model.workspace
+            page = model.workspace.page
+        in 
+            ({ model | workspace = { workspace | page = { page | name = name } } }, Cmd.none)
+    UpdatePageRoute route ->
+        let 
+            workspace = model.workspace
+            page = model.workspace.page
+        in 
+            ({ model | workspace = { workspace | page = { page | route = route } } }, Cmd.none)
+    SavePage page ->
+        let 
+            saver
+            = Task.perform HttpFail HttpSucceedSavePage 
+              <| Http.post 
+              <| Json.succeed Nothing
+              |> localhost ++ "pile/page" 
+              -- TODO: { _id: null, pagelet_id: null, name: null, route: null }
+              <| Http.empty
+        in
+            (model, saver)
+    SavePagelet pagelet ->
+        let 
+            saver
+            = Task.perform HttpFail HttpSucceedSavePagelet
+              <| Http.post 
+              <| Json.succeed Nothing
+              |> localhost ++ "pile/pagelet" 
+              -- TODO: { _id: null, pagelet_ids: null, scrap_id: null, options: {} }
+              <| Http.empty
+        in
+            (model, saver)
+    DragScrap scrap ->
+        let 
+            workspace = model.workspace
+        in 
+            ({ model | workspace = { workspace | hoverscrap = scrap } }, Cmd.none)
+    DropScrap scrap ->
+        case overpagelets of model.workspace.overpagelets
+            [] ->
+                (model, Cmd.none)
+            overpagelet::_ ->
+                let 
+                    workspace = model.workspace
+                in 
+                    ({ model | workspace = { workspace | hoverscrap = Nothing, pagelet = Pagelet.setScrap overpagelet scrap } }, Cmd.none)
+    MouseEnterPagelet pagelet ->
+        case maybescrap of model.workspace.hoverscrap
+            Nothing ->
+                ({ model | workspace = { model.workspace | overpagelets = pagelet :: model.workspace.overpagelets } }, Cmd.none)
+            Just hoverscrap ->
+                ({ model | workspace = { model.workspace | overpagelets = pagelet :: model.workspace.overpagelets, pagelet = pagelet } }, Cmd.none)
+    MouseLeavePagelet pagelet ->
+        ({ model | workspace = { model.workspace | overpagelets = filter (!= pagelet) model.workspace.overpagelets } }, Cmd.none)
     -- KLUDGE: shouldn't be chaining the commands like this
-    FetchPageSucceed pages ->
-      ({ model | pages = pages }, Debug.log "FetchPageSucceed" fetchScraps)
-    FetchScrapSucceed htmls ->
-      ({ model | workspace = div [ styleColumn 50 "white" ] htmls }, Debug.log "FetchScrapSucceed" Cmd.none)
-    FetchFail error ->
-      case error of
-          Http.Timeout ->
-              (model, Debug.log "timeout" Cmd.none)
-          Http.NetworkError ->
-              (model, Debug.log "network error" Cmd.none)
-          Http.UnexpectedPayload stuff ->
-              (model, Debug.log stuff Cmd.none)
-          Http.BadResponse code stuff ->
-              (model, Debug.log (stuff ++ toString code) Cmd.none)
-              
+    HttpSucceedSavePage page ->
+        ({ model | workspace = { model.workspace | page = page } }, Debug.log "FetchPageSucceed" Cmd.none)
+    HttpSucceedSavePagelet page ->
+        ({ model | workspace = { model.workspace | pagelet = pagelet } }, Debug.log "FetchPageSucceed" Cmd.none)
+    HttpSucceedFetchPages pages ->
+        ({ model | pages = pages }, Debug.log "FetchPageSucceed" Cmd.none)
+    HttpSucceedFetchScraps htmls ->
+        ({ model | workspace = div [ styleColumn 50 "white" ] htmls }, Debug.log "FetchScrapSucceed" Cmd.none)
+    HttpFail error ->
+        case error of
+            Http.Timeout ->
+                (model, Debug.log "timeout" Cmd.none)
+            Http.NetworkError ->
+                (model, Debug.log "network error" Cmd.none)
+            Http.UnexpectedPayload stuff ->
+                (model, Debug.log stuff Cmd.none)
+            Http.BadResponse code stuff ->
+                (model, Debug.log (stuff ++ toString code) Cmd.none)
 
 
 -- SUBSCRIPTIONS
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
-
+-- TODO: subscribe to pages
+-- TODO: subscribe to scraps
+-- TODO: subscribe to mouse down
 
 -- VIEW
 
-styleColumn : a -> String -> Attribute b
-styleColumn width color = 
-  style 
-    [ "float" => "left"
-    , "margin" => "0"
-    , "overflow" => "auto"
-    , "width" => ( (toString width) ++ "%" )
-    , "height" => "100%"
-    , "background-color" => color
-    ]
-
-view : Model -> Html Msg
-view model =
-  div [ style [ "height" => "100%" ] ] 
-    [ pageNav model.pages
-    , pageDetails model.page
-    -- , pageBuild model.pagelet model.page.pagelets
-    , model.workspace
-    , pageletDetails model.pagelet model.scraps 
-    ]
-      
-pageNav : List (Page Msg) -> Html Msg
-pageNav pages =
-  aside [ styleColumn 15 "#E8E8E8" ]
-    [ ul []
-        (List.map pageLink pages)
-    ]
-      
-pageLink : Page Msg -> Html Msg
-pageLink page =
-    li [] [ a [onClick (ViewPage page), href "#"] [ text page.name ] ]
-
-pageDetails : Page Msg -> Html Msg
-pageDetails page =
-  article [ styleColumn 10 "#EEE" ] 
-    [ ul []
-        [ li [] [ text page.name ]
-        , li [] [ text page.route ]
-        ]
-    ]
-      
-render : Pagelet Msg -> Html Msg
-render pagelet =
-  (.html (.scrap (pick pagelet)))
-
-pageletBuild : Pagelet Msg -> Pagelet Msg -> Html Msg
-pageletBuild the_pagelet a_pagelet =
-  section [ onClick (SelectPagelet a_pagelet), style [ "background-color" => ( if a_pagelet == the_pagelet then "#EEE" else "white" ) ] ] [ render a_pagelet ]
-
-pageBuild : Pagelet Msg -> List (Pagelet Msg) -> Html Msg
-pageBuild the_pagelet pagelets =
-  article [ styleColumn 50 "white" ] (List.map (pageletBuild the_pagelet) pagelets)
-      
-pageletDetails : Pagelet Msg -> List (Scrap Msg) -> Html Msg
-pageletDetails pagelet scraps =
-  aside [ styleColumn 25 "#EEE" ]
-    [ h2 [] [ text (.name (.scrap (pick pagelet))) ]
-    , if (String.isEmpty (.name (.scrap (pick pagelet)))) then (ul [] []) else scrapsNav scraps
-    ]
-      
-scrapsNav : List (Scrap Msg) -> Html Msg
-scrapsNav scraps =
-    ul [] (List.map scrapLink scraps)      
-
-scrapLink : Scrap Msg -> Html Msg
-scrapLink scrap =
-    li [] [ a [ onClick (SelectScrap scrap), href "#" ] [ text scrap.name ] ]
-
-
--- HTTP
-
-fetchPages : Cmd Msg
-fetchPages =
-    let
-        url = "http://taysar.com:9097/pile/page"
-    in
-        Task.perform FetchFail FetchPageSucceed 
-        <| Http.get JsonToPage.parseJson url
-            
-fetchScraps : Cmd Msg
-fetchScraps = 
-    let
-        url = "http://taysar.com:9097/scrap/json/taysar"
-        body = Http.empty
-    in
-        Task.perform FetchFail FetchScrapSucceed 
-        <| Http.post JsonToHtml.parseJson url body
-        
+-- TODO: all this crap
+-- TODO: remember that scraps are supposed to be functions that generate html blah blah blah
--- elm-src/Scrap.elm
+++ elm-src/Scrap.elm
@@ -3,10 +3,8 @@ port module Scrap exposing (..)
 
 import Html exposing (..)
 
--- TODO: scrap.html really contains a route to the scrap, which accepts html/arguments and returns html
--- TODO: pass it a "blank" scrap for it to render in the empty spot
--- TODO: holes may not be visible in the viewer, so users should add subscraps in the scrap detail area
--- TODO: OR OR OR we can pass the actual scrap functions to the elm code on the server before sending
+-- TODO: use IDs from mongo
+-- TODO: use HtmlToScrap to make a function ((Html msg) -> (Html msg))
 type alias Scrap msg =
     { name : String
     , html : Html msg
